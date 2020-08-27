55c91c1ed47efe0879a7367b398696892417aad4
piyush-kumar-au9 <piyush.16bcon546@jecrcu.edu.in>
2020-08-27 14:14:29 +0530

    Initail Stage of React App--import

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
+// import logo from './logo.svg';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+/* To import our Wall Component */
+import Wall from "./Components/Wall/wall.js"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    // React Component always in TitleCase or they start with Capital Alphabets to differ from HTML Components
+    <Wall/>
+
   );
 }
 
